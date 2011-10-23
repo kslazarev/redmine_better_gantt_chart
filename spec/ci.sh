558b4399d28d6ff3031cf2b091d4d05bf7deb263
Alexey Kuleshov <kulesa@gmail.com>
2011-10-23 16:16:23 +0400

    CI gotcha - added missing database.yml for ci environment

--- spec/ci.sh
+++ spec/ci.sh
@@ -1,5 +1,5 @@
 echo "Setting up dummy Redmine"
 git clone https://github.com/edavis10/redmine.git spec/dummy_redmine 
 cd spec/dummy_redmine && git co 1.1.3
-cp spec/ci_config/* spec/dummy_redmine/config/  
+cp spec/ci_config/database.ci.yml spec/dummy_redmine/config/database.yml  
 cd spec/summy/redmine && bundle exec rake db:create db:migrate 
