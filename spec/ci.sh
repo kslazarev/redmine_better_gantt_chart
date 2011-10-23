30bc86ee2c4dbee4977f7cedf1cc8fb8cc2a5e4c
Alexey Kuleshov <kulesa@gmail.com>
2011-10-23 16:06:35 +0400

    Changes in CI config again

--- spec/ci.sh
+++ spec/ci.sh
@@ -1,2 +1,5 @@
-#!/usr/bin/env sh
-cd spec && git clone https://github.com/edavis10/redmine.git dummy_redmine && cp ci_config/database.yml dummy_redmine/config/database.yml && cd dummy_redmine && git co 1.1.3 && bundle exec rake db:create db:migrate && cd ../.. && bundle exec rake spec
+echo "Setting up dummy Redmine"
+git clone https://github.com/edavis10/redmine.git spec/dummy_redmine 
+cd spec/dummy_redmine && git co 1.1.3
+cp spec/ci_config/* spec/dummy_redmine/config/  
+cd spec/summy/redmine && bundle exec rake db:create db:migrate 
