58b88c20947414fc480e88c19516dd04d5f083fd
Alexey Kuleshov <kulesa@gmail.com>
2011-10-23 16:27:31 +0400

    Come on TravisCI!

--- spec/ci.sh
+++ spec/ci.sh
@@ -1,5 +1,5 @@
 echo "Setting up dummy Redmine"
 git clone https://github.com/edavis10/redmine.git spec/dummy_redmine 
-cd spec/dummy_redmine && git co 1.1.3
-cp spec/ci_config/database.ci.yml spec/dummy_redmine/config/database.yml  
-cd spec/summy/redmine && bundle exec rake db:create db:migrate 
+cd spec/dummy_redmine && git checkout 1.1.3
+cp ci_config/database.ci.yml dummy_redmine/config/database.yml  
+cd dummy_redmine && bundle exec rake db:create db:migrate 
