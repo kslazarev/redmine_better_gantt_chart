99e3657512f594a66e439fcf2071f1fd65bf1a53
Alexey Kuleshov <kulesa@gmail.com>
2011-10-24 16:49:26 +0400

    Pursuing CI - fixed rubytree gem version, changed all the scripts

--- spec/ci.sh
+++ spec/ci.sh
@@ -2,6 +2,10 @@ echo "Setting up dummy Redmine"
 git clone https://github.com/edavis10/redmine.git spec/dummy_redmine 
 echo "Checking out branch 1.2"
 cd spec/dummy_redmine && git checkout origin/1.2-stable
+echo "Copying database.yml"
+cp ../ci_config/database.ci.yml config/database.yml  
+echo "Migrating database"
+RAILS_ENV=test bundle exec rake db:create db:migrate 
 cd ../..
-cp spec/ci_config/database.ci.yml spec/dummy_redmine/config/database.yml  
-cd spec/dummy_redmine && bundle exec rake db:create db:migrate 
+echo "Cloning the plugin to dummy Redmine plugins folder"
+git clone . spec/dummy_redmine/vendor/plugins/redmine_better_gantt_chart
