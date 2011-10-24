250b0a152d0ec2ec1d12fa509277ba1530e601a7
Alexey Kuleshov <kulesa@gmail.com>
2011-10-24 14:00:19 +0400

    Changed CI script and Gemfile

--- spec/ci.sh
+++ spec/ci.sh
@@ -1,7 +1,7 @@
 echo "Setting up dummy Redmine"
 git clone https://github.com/edavis10/redmine.git spec/dummy_redmine 
+echo "Checking out branch 1.2"
 cd spec/dummy_redmine && git checkout origin/1.2-stable
-bundle exec rake gems:install
 cd ../..
 cp spec/ci_config/database.ci.yml spec/dummy_redmine/config/database.yml  
 cd spec/dummy_redmine && bundle exec rake db:create db:migrate 
