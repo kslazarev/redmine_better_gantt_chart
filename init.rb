a866e711f5065cb334f9d8ca8ab6bbbc74a98655
kslazarev <k.s.lazarev@gmail.com>
2012-03-12 17:26:34 +0400

    Restricted access.

--- init.rb
+++ init.rb
@@ -29,6 +29,10 @@ Dispatcher.to_prepare :redmine_issue_dependency do
   end
 end
 
+require 'redmine_better_gantt_chart/redmine_better_gantt_chart'
+require 'redmine_better_gantt_chart/calendar'
+require 'redmine_better_gantt_chart/hooks/view_issues_show_details_bottom_hook'
+
 Redmine::Plugin.register :redmine_better_gantt_chart do
   name 'Redmine Better Gantt Chart plugin'
   author 'Alexey Kuleshov'
@@ -46,10 +50,6 @@ Redmine::Plugin.register :redmine_better_gantt_chart do
     'work_on_weekends' => true
   }, :partial => "settings/better_gantt_chart_settings")
 
-  permission :time_balances, {:time_balances => [:index, :vote]}, :public => true
-  menu :project_menu, :time_balances, {:controller => 'time_balances', :action => 'index'}, :caption => 'Time balances', :after => :gantt, :param => :project_id
+  permission :view_time_periods_statistics, {:time_balances => [:index, :vote]}
+  menu :project_menu, :time_balances, {:controller => 'time_balances', :action => 'index'}, :caption => :time_balances, :after => :gantt, :param => :project_id
 end
-
-require 'redmine_better_gantt_chart/redmine_better_gantt_chart'
-require 'redmine_better_gantt_chart/calendar'
-require 'redmine_better_gantt_chart/hooks/view_issues_show_details_bottom_hook'
